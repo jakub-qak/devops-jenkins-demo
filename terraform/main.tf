c08662390316c51ffece9304f1903ae06142b369
jakub-qak <kubakwak75@gmail.com>
2024-10-31 23:29:59 +0100

    Attach key pair and add security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,34 @@
 resource "aws_instance" "jenkins" {
   ami           = "ami-005fc0f236362e99f"
   instance_type = "t2.micro"
+  key_name      = "jenkins_key"
 
   tags = {
     Name = "JenkinsServer"
+  }
+}
+
+# Create Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # All IPs allowed
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # All IPs allowed
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
